4aa618ef93c643f8eaa4478be5ba4e4e749862be
Andrew921as <andresfelipecoronado@gmail.com>
2025-05-20 22:38:12 -0500

    objects prototypes

---
+++ prototypeInheritance.js
@@ -0,0 +1,34 @@
+const cheff = {
+  type: 'Colombian food',
+  cook: function (dish){
+    console.log('cooking ',dish);
+  }
+}
+
+const andres = {
+  name: 'Andres',
+  age: 23,
+  career: 'Developer'
+}
+
+//To connect 2 objects:
+//hidden property and it shouldn't be used
+andres.__proto__ = cheff;
+
+//Also there is a function
+/**
+ * req: object to connect, prototype to connect
+ */
+Object.setPrototypeOf(andres,cheff);
+
+console.log(andres.name);
+console.log(andres.age);
+console.log(andres.cook('Sancocho'));
+
+
+// To know what is the prototype of a object:
+console.log(andres.__proto__=== cheff);
+
+//It can be done in the other way this method says that if the object appears in ANY place of the prototype chain of the object:
+console.log(cheff.isPrototypeOf(andres));
+console.log(Object.prototype.isPrototypeOf(andres));
